449ee1211f4283d6928902fd4bce386ff02aa90d
YYZwizard <trivediravi561@gmail.com>
2023-09-18 15:38:40 -0400

    Setting up connection

---
+++ lib/persona-ruby/connection.rb
@@ -0,0 +1,32 @@
+require 'faraday'
+
+module Persona
+  class Connection
+    def initialize(url, auth_token)
+      @connection = Faraday.new(url: url) do |builder|
+        builder.request :json
+        builder.headers[:accept] = 'application/json'
+        builder.response :json
+        builder.request :authorization, auth_token, ''
+      end
+    end
+
+    def delete(id, params = {})
+      request(:delete, id, params)
+    end
+
+    def get(id, params = {})
+      request(:get, id, params)
+    end
+
+    def post(id, params = {})
+      request(:post, id, params)
+    end
+
+    def request(method, id, params = {})
+      response = @connection.send(method, id, params)
+
+      response.body
+    end
+  end
+end
