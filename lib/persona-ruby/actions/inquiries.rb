462687b97fd20352f2860405b3b3074ef75c4330
YYZwizard <trivediravi561@gmail.com>
2023-09-22 14:55:59 -0400

    [OR-3712]: Create an inquiry

--- lib/persona-ruby/actions/inquiries.rb
+++ lib/persona-ruby/actions/inquiries.rb
@@ -10,6 +10,10 @@ module Persona
       def inquiry_get(id)
         connection.get("inquiries/#{id}", {})
       end
+
+      def inquiry_create(account_id, inquiry_template_id)
+        connection.post("inquiries", { data: { attributes: { "account-id": account_id, "inquiry-template-id": inquiry_template_id } } })
+      end
     end
   end
 end
--- spec/persona-ruby/actions/inquiries_spec.rb
+++ spec/persona-ruby/actions/inquiries_spec.rb
@@ -29,4 +29,18 @@ RSpec.describe Persona::Actions::Inquiries do
       expect(stub).to have_been_requested
     end
   end
+
+  describe "#inquiry_post" do
+    it "issues the correct POST request to create an inquiry" do
+      account_id = "act_123"
+      inquiry_template_id = "itmpl_123"
+      stub = stub_request(:post,
+                          "#{@client.url}/inquiries").with(body: { data: { attributes: { "account-id": account_id,
+                                                                                         "inquiry-template-id": inquiry_template_id } } })
+
+      @client.inquiry_create(account_id, inquiry_template_id)
+
+      expect(stub).to have_been_requested
+    end
+  end
 end
